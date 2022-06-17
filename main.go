76c82bc759a64a137ab68952c113307943901731
Asai Toshiya <to.asai.60@gmail.com>
2022-06-17 22:52:05 +0900

    「Open...」メニュー項目を追加する

--- main.go
+++ main.go
@@ -1,17 +1,33 @@
 package main
 
 import (
+	"io"
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/dialog"
+	"fyne.io/fyne/v2/storage"
 	"fyne.io/fyne/v2/widget"
 )
 
+var w fyne.Window
 var input = widget.NewMultiLineEntry()
 
 func createFileMenu() *fyne.Menu {
 	newMenuItem := fyne.NewMenuItem("New", func() { input.SetText("") })
+	openMenuItem := fyne.NewMenuItem("Open...", func() {
+		fd := dialog.NewFileOpen(
+			func(r fyne.URIReadCloser, _ error) {
+				if r != nil {
+					b, _ := io.ReadAll(r)
+					input.SetText(string(b))
+				}
+			}, w)
+		fd.SetFilter(storage.NewExtensionFileFilter([]string{".txt"}))
+		fd.Show()
+	})
 	fileMenu := fyne.NewMenu("File",
 		newMenuItem,
+		openMenuItem,
 	)
 	return fileMenu
 }
@@ -25,7 +41,7 @@ func createMenus() *fyne.MainMenu {
 
 func main() {
 	a := app.New()
-	w := a.NewWindow("Hello World")
+	w = a.NewWindow("Hello World")
 	w.Resize(fyne.NewSize(1366, 768))
 
 	w.SetMainMenu(createMenus())
